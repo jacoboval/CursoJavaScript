ea31c0588fcb072c2fb5ef4718a2d972f1e4a960
Jacobo valencia <jacoboval@gmail.com>
2025-02-19 02:38:27 -0600

    for

--- JavaScriptParaWeb-CreaPaginasDinámicas/Aula1/AluraMidi-Curso-main/main.js
+++ JavaScriptParaWeb-CreaPaginasDinámicas/Aula1/AluraMidi-Curso-main/main.js
@@ -2,21 +2,24 @@ function playSonido(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
-while(contador < 9){
+
+
+for(let contador = 0 ; contador < listaDeTeclas.length ; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];  // claslist -> class=[tecla] [tecla_puff]  
-    //console.log(instrumento);
-
     const idAudio = `#sonido_${instrumento}`;  // id="sonido_tecla_pom"
-    //console.log(idAudio);
-
+ 
     tecla.onclick = function(){//  funcion generica
         playSonido(idAudio);
     };
-    contador = contador + 1;
-    //console.log('Vuelta ' + contador);
+    tecla.onkeydown = function(evento){
+        
+        if(evento.code ==='Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa')
+        }        
+    }
+    
+    tecla.onkeyup = function(){
+        tecla.classList.remove('activa')
+    }
 }
-
-// listaDeTeclas[1].onclick = playSonidoPom; */
-//document.querySelector('.tecla_pom').onclick = playSonidoPom;
